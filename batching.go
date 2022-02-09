3a6eb4bc13101037e648318262bd34447e79f7fe
Gus Eggert <gus@gus.dev>
2022-02-09 16:17:30 -0500

    Upgrade to go-datastore@v0.5.1

--- batching.go
+++ batching.go
@@ -14,11 +14,11 @@ type batch struct {
 }
 
 // Batch creates a set of deferred updates to the database.
-func (d *Datastore) Batch() (ds.Batch, error) {
+func (d *Datastore) Batch(_ context.Context) (ds.Batch, error) {
 	return &batch{ds: d, batch: &pgx.Batch{}}, nil
 }
 
-func (b *batch) Put(key ds.Key, value []byte) error {
+func (b *batch) Put(ctx context.Context, key ds.Key, value []byte) error {
 	b.batch.Queue("BEGIN")
 	sql := fmt.Sprintf("INSERT INTO %s (key, data) VALUES ($1, $2) ON CONFLICT (key) DO UPDATE SET data = $2", b.ds.table)
 	b.batch.Queue(sql, key.String(), value)
@@ -26,18 +26,14 @@ func (b *batch) Put(key ds.Key, value []byte) error {
 	return nil
 }
 
-func (b *batch) Delete(key ds.Key) error {
+func (b *batch) Delete(ctx context.Context, key ds.Key) error {
 	b.batch.Queue("BEGIN")
 	b.batch.Queue(fmt.Sprintf("DELETE FROM %s WHERE key = $1", b.ds.table), key.String())
 	b.batch.Queue("COMMIT")
 	return nil
 }
 
-func (b *batch) Commit() error {
-	return b.CommitContext(context.Background())
-}
-
-func (b *batch) CommitContext(ctx context.Context) error {
+func (b *batch) Commit(ctx context.Context) error {
 	res := b.ds.pool.SendBatch(ctx, b.batch)
 	defer res.Close()
 
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/ipfs/ipfs-ds-postgres
 go 1.14
 
 require (
-	github.com/ipfs/go-datastore v0.4.4
+	github.com/ipfs/go-datastore v0.5.1
 	github.com/jackc/pgx/v4 v4.6.0
 )
